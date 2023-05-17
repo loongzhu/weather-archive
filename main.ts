a144815f5f229ad3e35d7c76fed5075f1ddf654b
zhuzhenquan <2332778528@qq.com>
2023-05-17 21:29:04 +0800

    upgrade: get city code

--- main.ts
+++ main.ts
@@ -1,9 +1,28 @@
-import { getCitys, formatDate } from "./api.ts";
+import { getCitys, formatDate, flattenObject, writeFile } from "./api.ts";
+import { CityCode } from "./types.ts";
 
-// const city_data = await getCitys();
+const city_data = await getCitys();
 
 // console.log("🚀 ~ city_data:", city_data);
 
-const date = formatDate();
+// const date = formatDate();
 
-console.log("🚀 ~ date:", date + ".json");
+// console.log("🚀 ~ date:", date + ".json");
+
+const city_codes = flattenObject(city_data);
+
+const city_arr = ["北京", "上海", "天津", "重庆", "沧州"];
+
+const citys: CityCode = Object.keys(city_codes).reduce(
+  (obj: CityCode, key: string) => {
+    if (city_arr.includes(key)) {
+      obj[key] = city_codes[key];
+    }
+    return obj;
+  },
+  {}
+);
+
+console.log("🚀 ~ citys:", citys);
+
+writeFile("flattened", city_codes);
