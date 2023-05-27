09204003a3dd8c9240c828eb2eea21d74ab2e39c
zhuzhenquan <2332778528@qq.com>
2023-05-27 16:23:10 +0800

    fix: add try catch

--- main.ts
+++ main.ts
@@ -18,40 +18,48 @@ if (node_version < "18.0.0") {
   throw new Error("node version must >= 18.0.0");
 }
 
-const city_data = await getCities();
+function main() {
+  try {
+    const city_data = await getCities();
 
-const city_codes = flattenObject(city_data);
+    const city_codes = flattenObject(city_data);
 
-const capitals = getCapitals(city_data);
+    const capitals = getCapitals(city_data);
 
-const cities: CityData[] = capitals.map((capital) => {
-  const city = {
-    id: city_codes[capital.city],
-    province: capital.province,
-    city: capital.city,
-  };
-  return city;
-});
+    const cities: CityData[] = capitals.map((capital) => {
+      const city = {
+        id: city_codes[capital.city],
+        province: capital.province,
+        city: capital.city,
+      };
+      return city;
+    });
 
-for (let city of cities) {
-  city.realtime = await getRealtimeData(city.id);
-  city.weather = await getWeatherData(city.id);
+    for (let city of cities) {
+      city.realtime = await getRealtimeData(city.id);
+      city.weather = await getWeatherData(city.id);
+    }
+
+    const today = new Date();
+    const { date, datetime }: dateType = await getDate(today);
+    const fDate = formatDate(today);
+
+    const content = {
+      timestamp: today.getTime(),
+      lastUpdate: datetime,
+      data: cities,
+    };
+
+    await mkdir(`weathers/${date}`);
+
+    await writeFile(`weathers/${date}/${fDate}.json`, content);
+
+    await writeFile(`weathers/${date}/latest.json`, content);
+
+    await writeFile(`weathers/latest.json`, content);
+  } catch (err: any) {
+    main();
+  }
 }
 
-const today = new Date();
-const { date, datetime }: dateType = await getDate(today);
-const fDate = formatDate(today);
-
-const content = {
-  timestamp: today.getTime(),
-  lastUpdate: datetime,
-  data: cities,
-};
-
-await mkdir(`weathers/${date}`);
-
-await writeFile(`weathers/${date}/${fDate}.json`, content);
-
-await writeFile(`weathers/${date}/latest.json`, content);
-
-await writeFile(`weathers/latest.json`, content);
+main();
